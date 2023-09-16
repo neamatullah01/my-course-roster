259326cacf6813d8a99511d6aa2145534ec4b029
neamatullah01 <neamat22205101908@diu.edu.bd>
2023-09-16 20:32:37 +0600

    Remaining credit hour added in cart

--- src/components/Carts/Carts.jsx
+++ src/components/Carts/Carts.jsx
@@ -2,16 +2,16 @@ const Carts = ({carts, totalCredit, remainingCredit}) => {
     return (
         <div className="w-full h-auto bg-base-100 shadow-xl rounded-lg">
             <div>
-                <p className="px-4 py-4 border-b-2">Credit Hour Remaining</p>
+                <p className="px-4 py-4 border-b-2 text-lg font-bold text-blue-700">Credit Hour Remaining {remainingCredit} hr</p>
             </div>
-            <h1 className="px-4 py-1 text-xl font-bold">Course Name:</h1>
+            <h1 className="px-4 py-1 text-xl font-bold">Course Name</h1>
             <div className="px-4 py-4">
                 {
                     carts.map((course) => <ol>{course.course_name}</ol>)
                 }
             </div>
-            <div>
-                <p>Total Credit Hours: {totalCredit}</p>
+            <div className="px-4 py-4 border-t-2">
+                <p className="text-base font-medium">Total Credit Hour: {totalCredit}</p>
             </div>
         </div>
     );
